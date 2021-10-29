4ed3db5f8bef2cbd8a184cc01c9360b51e5a317a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-29 11:52:56 -0500

    initial version

--- SwiftUI-MatchedGeometryEffect/ContentView.swift
+++ SwiftUI-MatchedGeometryEffect/ContentView.swift
@@ -1,16 +1,56 @@
-//
-//  ContentView.swift
-//  SwiftUI-MatchedGeometryEffect
-//
-//  Created by R. Mark Volkmann on 10/29/21.
-//
-
 import SwiftUI
 
+struct Food: Identifiable {
+    var name: String
+    var selected: Bool = false
+    var id: String { name }
+}
+
 struct ContentView: View {
+    @Namespace private var foodNS
+    
+    @State var foods: [Food] = [
+        Food(name: "Hamburger"),
+        Food(name: "Fries"),
+        Food(name: "Shake"),
+    ]
+    
+    func toggle(food: Food) {
+        let index = foods.firstIndex(where: { $0.id == food.id })!
+        foods[index].selected.toggle()
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            VStack {
+                Text("Available")
+                List {
+                    ForEach(foods) { food in
+                        if !food.selected {
+                            Text(food.name)
+                                .matchedGeometryEffect(id: food.name, in: foodNS)
+                                .onTapGesture {
+                                    withAnimation { toggle(food: food) }
+                                }
+                        }
+                    }
+                }
+            }
+            VStack {
+                Text("Selected")
+                List {
+                    ForEach(foods) { food in
+                        if food.selected {
+                            Text(food.name)
+                                .matchedGeometryEffect(id: food.name, in: foodNS)
+                                .onTapGesture {
+                                    withAnimation { toggle(food: food) }
+                                }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
