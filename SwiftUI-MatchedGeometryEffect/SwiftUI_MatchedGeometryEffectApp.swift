c833ced40e76fe12f215cb65f05de1c5aa8f88c9
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-29 11:36:54 -0500

    Initial Commit

---
+++ SwiftUI-MatchedGeometryEffect/SwiftUI_MatchedGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MatchedGeometryEffectApp.swift
+//  SwiftUI-MatchedGeometryEffect
+//
+//  Created by R. Mark Volkmann on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MatchedGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
